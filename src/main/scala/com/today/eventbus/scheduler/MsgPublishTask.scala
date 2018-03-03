89ed2c53ab29a1aeb1509ffab7b16041d8ec5ad8
Ever <439674061@qq.com>
2018-03-03 17:54:29 +0800

    代码调整

--- src/main/scala/com/today/eventbus/scheduler/MsgPublishTask.scala
+++ src/main/scala/com/today/eventbus/scheduler/MsgPublishTask.scala
@@ -1,10 +1,10 @@
-package com.today.eventbus.scheduled
+package com.today.eventbus.scheduler
 
 import java.util.concurrent.atomic.AtomicInteger
 import javax.sql.DataSource
 
 import com.today.eventbus.MsgKafkaProducer
-import com.today.eventbus.dao.{EventMsgDao, EventStore}
+import com.today.eventbus.dao.EventStore
 import org.slf4j.LoggerFactory
 import wangzx.scala_commons.sql._
 
@@ -15,14 +15,12 @@ import wangzx.scala_commons.sql._
   * @author hz.lei
   * @date 2018年02月28日 下午3:00
   */
-class MsgPublishTimer(topic: String, host: String, transId: String, msgDao: EventMsgDao, dataSource: DataSource) {
-  val eventMsgDao: EventMsgDao = msgDao
-  val producerTopic: String = topic
-  val kafkaServerHost: String = host
-  val ds: DataSource = dataSource
-
-  val logger = LoggerFactory.getLogger(classOf[MsgPublishTimer])
-  var producer = MsgKafkaProducer(kafkaServerHost, transId)
+class MsgPublishTask(topic: String,
+                     kafkaHost: String,
+                     transId: String,
+                     dataSource: DataSource) {
+  val logger = LoggerFactory.getLogger(classOf[MsgPublishTask])
+  var producer = MsgKafkaProducer(kafkaHost, transId)
 
   /**
     * fetch message from database , then send to kafka broker
@@ -32,31 +30,21 @@ class MsgPublishTimer(topic: String, host: String, transId: String, msgDao: Even
       logger.debug("begin the publish_msg time scheduler ")
     }
 
-    //    val eventStores: List[EventStore] = eventMsgDao.listMessages
-    //    if (!eventStores.isEmpty) {
-    //      eventStores.foreach(es => {
-
     val counter = new AtomicInteger(100)
     while (counter.get() == 100) {
       counter.set(0)
-      ds.withTransaction[Unit](conn => {
+      dataSource.withTransaction[Unit](conn => {
         conn.eachRow[EventStore](sql"SELECT * FROM common_event limit 100 FOR UPDATE")(event => {
           conn.executeUpdate(sql"DELETE FROM common_event WHERE id = ${event.id}")
-          producer.send(producerTopic, event.id, event.eventBinary)
+          producer.send(topic, event.id, event.eventBinary)
           counter.incrementAndGet()
         })
       })
     }
 
-
-    //        })
-    //      })
     if (logger.isDebugEnabled()) {
       logger.debug("send listMessages successful ")
     }
-    //    } else {
-    //      logger.debug("database no message to process ")
-    //    }
   }
 
 }
