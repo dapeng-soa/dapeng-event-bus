09d937b77b796a9c036820ab000abdcf21f4f01a
Ever <439674061@qq.com>
2018-03-04 11:13:28 +0800

    代码调整

--- src/main/scala/com/today/eventbus/EventStore.scala
+++ src/main/scala/com/today/eventbus/EventStore.scala
@@ -1,7 +1,5 @@
 package com.today.eventbus
 
-import java.sql.Timestamp
-
 /**
   *
   * 描述: 消息持久化 case class
@@ -9,4 +7,4 @@ import java.sql.Timestamp
   * @author hz.lei
   * @date 2018年02月28日 下午2:50
   */
-case class EventStore(id: Long, eventType: String, eventBinary: Array[Byte], updatedAt: Timestamp)
+case class EventStore(id: Long, eventType: String, eventBinary: Array[Byte])
--- src/main/scala/com/today/eventbus/scheduler/MsgPublishTask.scala
+++ src/main/scala/com/today/eventbus/scheduler/MsgPublishTask.scala
@@ -1,52 +1,57 @@
 package com.today.eventbus.scheduler
 
+import java.util.UUID
 import java.util.concurrent.atomic.AtomicInteger
 import javax.sql.DataSource
 
 import com.today.eventbus.{EventStore, MsgKafkaProducer}
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
 import wangzx.scala_commons.sql._
 
 /**
   *
   * 描述: 事件定时任务，轮询数据库，发送消息 to kafka
   *
-  * transId: kafka消息对事务的支持前提，需要每一个生产者实例有不同的事务ID，唯一
-  *
   * @author hz.lei
   * @date 2018年02月28日 下午3:00
   */
 class MsgPublishTask(topic: String,
                      kafkaHost: String,
-                     transId: String,
+                     tidPrefix: String,
                      dataSource: DataSource) {
-  val logger: Logger = LoggerFactory.getLogger(classOf[MsgPublishTask])
-  var producer = MsgKafkaProducer(kafkaHost, transId)
+  private val logger = LoggerFactory.getLogger(classOf[MsgPublishTask])
+  //transId: kafka消息对事务的支持前提，需要每一个生产者实例有不同的事务ID，全局唯一
+  private val tid = tidPrefix + UUID.randomUUID().toString
+  private val producer = MsgKafkaProducer(kafkaHost, tid)
+  logger.warn("Kafka producer transactionId:" + tid)
 
   /**
     * fetch message from database , then send to kafka broker
     */
   def doPublishMessages(): Unit = {
-    if (logger.isDebugEnabled()) {
-      logger.debug("begin to publish messages to kafka")
-    }
+    logger.info("begin to publish messages to kafka")
 
-    //TODO comments
-    val counter = new AtomicInteger(100)
-    while (counter.get() == 100) {
-      counter.set(0)
+    // 消息总条数计数器
+    val counter = new AtomicInteger(0)
+    // 批量处理, 每次从数据库取出消息的最大数量(window)
+    val window = 100
+    // 单轮处理的消息计数器, 用于控制循环退出.
+    val resultSetCounter = new AtomicInteger(window)
+
+    while (resultSetCounter.get() == window) {
+      resultSetCounter.set(0)
       dataSource.withTransaction[Unit](conn => {
-        conn.eachRow[EventStore](sql"SELECT * FROM common_event limit 100 FOR UPDATE")(event => {
+        conn.eachRow[EventStore](sql"SELECT * FROM common_event limit ${window} FOR UPDATE")(event => {
           conn.executeUpdate(sql"DELETE FROM common_event WHERE id = ${event.id}")
           producer.send(topic, event.id, event.eventBinary)
+          
+          resultSetCounter.incrementAndGet()
           counter.incrementAndGet()
         })
       })
     }
 
-    if (logger.isDebugEnabled()) {
-      logger.debug("end publish messages to kafka")
-    }
+    logger.info(s"end publish messages(${counter.get()}) to kafka")
   }
 
 }
