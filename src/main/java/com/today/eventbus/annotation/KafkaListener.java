8790288a56da79b545821ddce9411c4eff43ad8d
leihuazhe <hzlei@today36524.com.cn>
2018-03-05 16:58:22 +0800

    fix conflict

--- src/main/java/com/today/eventbus/annotation/KafkaListener.java
+++ src/main/java/com/today/eventbus/annotation/KafkaListener.java
@@ -3,12 +3,12 @@ package com.today.eventbus.annotation;
 import java.lang.annotation.*;
 
 /**
- * 描述:
+ * 描述: 方法上注解，标明此方法是一个kafka message 消息监听器
  *
  * @author hz.lei
  * @date 2018年03月02日 上午12:48
  */
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
