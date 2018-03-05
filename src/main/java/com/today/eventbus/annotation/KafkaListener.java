b3e15c15af9613aac638349b9c1f66f4dc4be5a8
leihuazhe <hzlei@today36524.com.cn>
2018-03-05 16:51:19 +0800

    新增一个类上注解

--- src/main/java/com/today/eventbus/annotation/KafkaListener.java
+++ src/main/java/com/today/eventbus/annotation/KafkaListener.java
@@ -1,8 +1,5 @@
 package com.today.eventbus.annotation;
 
-
-import com.today.eventbus.utils.Constant;
-
 import java.lang.annotation.*;
 
 /**
@@ -17,20 +14,6 @@ import java.lang.annotation.*;
 @Documented
 public @interface KafkaListener {
 
-    String topic() default "";
-
-
-    String groupId() default "";
-
-    String serializer() default "";
-
-    /**
-     * hosts for kafka cluster,  users should set the real host by
-     * System.setProperty(kafkaHostKey,"xxx") or export the env with the key.
-     *
-     * @return
-     */
-    String kafkaHostKey() default Constant.DEFAULT_CONSUMER_HOST_KEY;
-
+    Class<?> serializer();
 
 }
--- src/main/java/com/today/eventbus/utils/Constant.java
+++ src/main/java/com/today/eventbus/utils/Constant.java
@@ -7,5 +7,5 @@ package com.today.eventbus.utils;
  * @date 2018年03月04日 下午9:13
  */
 public interface Constant {
-    String DEFAULT_CONSUMER_HOST_KEY = "dapeng.kafka.consumer.host";
+    String DEFAULT_CONSUMER_HOST_KEY = "kafka.consumer.host";
 }
