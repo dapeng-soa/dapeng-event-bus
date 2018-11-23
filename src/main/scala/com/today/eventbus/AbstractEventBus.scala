280c749c6c539e1fa4e2d6b2d47605c7772fa0f7
leihuazhe <295482300@qq.com>
2018-11-23 11:02:54 +0800

    生产者逻辑 ...

--- src/main/scala/com/today/eventbus/AbstractEventBus.scala
+++ src/main/scala/com/today/eventbus/AbstractEventBus.scala
@@ -32,7 +32,7 @@ trait AbstractEventBus {
     */
   def fireEvent(event: Any): Unit = {
     dispatchEvent(event)
-    persistenceEvent(event)
+    persistenceEvent(None, event)
   }
 
   /**
@@ -43,61 +43,72 @@ trait AbstractEventBus {
     */
   def fireEventManually(event: Any, conn: Connection): Unit = {
     dispatchEvent(event)
-    persistenceEventManually(event, conn)
+    persistenceEventManually(None, conn, event)
   }
 
   /**
-    * 业务系统处理事件分发逻辑
+    * 顺序的触发事件，需要多传入一个业务内容。
+    * 然后会根据这个内容将相同的tag消息发送到一个分区。
     *
-    * @param event biz custom event
+    * @since 3.0  2018.11.23
     */
-  protected def dispatchEvent(event: Any): Unit
+  def fireEventOrdered(biz: String, event: Any): Unit = {
+    dispatchEvent(event)
+    persistenceEvent(Option.apply(biz), event)
+  }
+
+  /**
+    * 顺序的触发事件，需要多传入一个业务内容。
+    * 然后会根据这个内容将相同的tag消息发送到一个分区。
+    * <p>
+    * 手动的事务.
+    * </p>
+    *
+    * @since 3.0  2018.11.23
+    */
+  def fireEventOrderedManually(biz: String, conn: Connection, event: Any): Unit = {
+    dispatchEvent(event)
+    persistenceEventManually(Option.apply(biz), conn, event)
+  }
+
 
   /**
     * 持久化 event 消息 to database
-    *
-    * @param event biz custom event
     */
-  @throws[TException]
-  private def persistenceEvent(event: Any): Unit = {
-    val processor = new KafkaMessageProcessor[Any]
-    val bytes: Array[Byte] = processor.encodeMessage(event)
-    val eventType = event.getClass.getName
-    // fetch id
-    val id = getMsgId(event)
-    val executeSql = sql"INSERT INTO  dp_common_event set id=${id}, event_type=${eventType}, event_binary=${bytes}"
+  private def persistenceEvent(biz: Option[String], event: Any): Unit = {
+    val (id, eventType, executeSql) = processSaveEvent(biz, event)
     dataSource.executeUpdate(executeSql)
-
-    logger.info(s"save message unique id: $id, eventType: $eventType successful ")
+    logger.info(s"save message unique id: $id, biz:$biz, eventType: $eventType successful ")
   }
 
   /**
     * 使用传入的 conn 对消息进行存储
-    *
-    * @param event
-    * @param conn
-    * @throws
     */
-  @throws[TException]
-  private def persistenceEventManually(event: Any, conn: Connection): Unit = {
-    logger.info("prepare to save event message with manually connection")
+  private def persistenceEventManually(biz: Option[String], conn: Connection, event: Any): Unit = {
+    val (id, eventType, executeSql) = processSaveEvent(biz, event)
+    conn.executeUpdate(executeSql)
+    logger.info(s"save message unique id: $id,biz: $biz, eventType: $eventType  successful with manually connection")
+  }
+
+  /**
+    * 公用 存储逻辑
+    */
+  private def processSaveEvent(biz: Option[String], event: Any): (Long, String, SQLWithArgs) = {
     val processor = new KafkaMessageProcessor[Any]
     val bytes: Array[Byte] = processor.encodeMessage(event)
     val eventType = event.getClass.getName
     // fetch id
     val id = getMsgId(event)
-    val executeSql = sql"INSERT INTO  dp_common_event set id=${id}, event_type=${eventType}, event_binary=${bytes}"
-    conn.executeUpdate(executeSql)
-
-    logger.info(s"save message unique id: $id, eventType: $eventType  successful with manually connection")
+    val executeSql = biz match {
+      case Some(value) ⇒ sql"INSERT INTO dp_common_event set id=${id}, event_type=${eventType}, event_biz=${value}, event_binary=${bytes}"
+      case None ⇒ sql"INSERT INTO  dp_common_event set id=${id}, event_type=${eventType}, event_binary=${bytes}"
+    }
+    (id, eventType, executeSql)
   }
 
   /**
     * Todo 临时解决方案
     * 反射拿到消息第一个id参数，作为存储数据库的 唯一 id
-    *
-    * @param event
-    * @return
     */
   private def getMsgId(event: Any): Long = {
     try {
@@ -110,6 +121,13 @@ trait AbstractEventBus {
         throw e
     }
   }
+
+  /**
+    * 业务系统处理事件分发逻辑
+    *
+    * @param event biz custom event
+    */
+  protected def dispatchEvent(event: Any): Unit
 
   /**
     * scala object 基于Spring的初始化方法
